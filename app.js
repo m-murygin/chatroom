9745e9523a277b32445d17e32e019f40acb9a56d
Maksim Murygin <muriginm@gmail.com>
2017-02-02 22:43:56 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const rooms = require('./data/room.json');
 
 const app = express();
 
@@ -8,11 +9,14 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Home' });
 });
 
 app.get('/admin/rooms', (req, res) => {
-  res.render('rooms');
+  res.render('rooms', {
+    rooms,
+    title: 'Admin Rooms',
+  });
 });
 
 app.listen(8080, () => {
