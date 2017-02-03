ab2d7e75fd4d432b2eb76959ddf40b83b21d57dc
Maksim Murygin <muriginm@gmail.com>
2017-02-03 08:15:06 +0700

    add adding room and logs

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const express = require('express');
+
+const log = require('./modules/logger.js');
 const rooms = require('./data/room.json');
 
 const app = express();
 
 app.set('view engine', 'pug');
+app.set('views', 'views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -19,6 +22,15 @@ app.get('/admin/rooms', (req, res) => {
   });
 });
 
+app.get('/admin/rooms/add', (req, res) => {
+  res.render('add_room', { title: 'add room' });
+});
+
+app.post('/admin/rooms/add', (req, res) => {
+  log.info(req);
+  res.send('nothing');
+});
+
 app.listen(8080, () => {
-  console.log('Example app listening on port 8080!');
+  log.info('Example app listening on port 8080!');
 });
