7c42d7991b0e8488aeec44cad9f888bc7a32f564
Maksim Murygin <muriginm@gmail.com>
2017-02-04 15:17:47 +0700

    add request time logging

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const log = require('./modules/logger.js');
 const adminRoutes = require('./routes/admin.js');
+const reponseTime = require('./middlewares/reponse-time.js');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
 // add static files middleware
 app.use(express.static('public'));
+
+app.use(reponseTime);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
